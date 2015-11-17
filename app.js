3edb74d08ba54b9630eb38bf3dddcc79c170637d
mcluo <mcluo@Ctrip.com>
2015-11-17 16:42:14 +0800

    something

--- app.js
+++ app.js
@@ -6,17 +6,9 @@ var path = require('path');
 var fs = require("fs");
 
 app.use(express.static("public"));
+app.use(express.static("bower_components"));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname + '/views'));
-
-app.post('/upload/multi', upload.array("photos", 12), function (req, res) {
-    var files = req.files;
-    files.forEach(function (file) {
-        var tail = "." + file.mimetype.split("/")[1];
-        fs.rename(file.path, file.path + tail);
-    });
-    res.send({status: true, files: req.files});
-});
 
 app.get("/multi", function (req, res) {
     res.render("multi");
@@ -26,6 +18,18 @@ app.get("/ajax", function (req, res) {
     res.render("ajax");
 });
 
+app.get("/dropzone", function (req, res) {
+    res.render("dropzone");
+});
+
+app.post('/upload/dropzone', upload.single("file"), function (req, res) {
+    var file = req.file;
+    var tail = "." + file.mimetype.split("/")[1];
+    fs.rename(file.path, file.path + tail, function (err) {
+        if (err) res.send({status: false});
+        else res.send({status: true, files: req.files});
+    });
+});
 
 app.post('/upload/ajax', upload.single("photos"), function (req, res) {
     var file = req.file;
@@ -34,6 +38,15 @@ app.post('/upload/ajax', upload.single("photos"), function (req, res) {
         if (err) res.send({status: false});
         else res.send({status: true, files: req.files});
     });
+});
+
+app.post('/upload/multi', upload.array("photos", 12), function (req, res) {
+    var files = req.files;
+    files.forEach(function (file) {
+        var tail = "." + file.mimetype.split("/")[1];
+        fs.rename(file.path, file.path + tail);
+    });
+    res.send({status: true, files: req.files});
 });
 
 var server = app.listen(process.env.PORT || 3333, function () {
